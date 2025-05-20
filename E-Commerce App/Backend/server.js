d7739ba1318c86a5f61721b50eb5fb4ca28c15f2
Sumit <sumit500241@gmail.com>
2025-05-20 13:28:17 +0530

    User authentication completed by backend

---
+++ E-Commerce App/Backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+
+// App Config
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// API Endpoints
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.use("/api/user", userRouter);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
